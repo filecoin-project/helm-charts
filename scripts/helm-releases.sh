d92ede58c7dfddaafb44171c63b272a8632afebc
ognots <ognots@protonmail.com>
2020-10-02 20:51:30 -0400

    bug fixes to ci publishing
    * rename circle config yaml extension
    * alter script to be resilient to cr errors, such as when a release
    already exists. also need to persist a valid index.yaml for cr index
    command. unsure why it seems to fetch the index.yaml from the github
    pages branch. need to keep an eye on future behaviour as we release new
    versions of charts

--- scripts/helm-releases.sh
+++ scripts/helm-releases.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 BRANCH=repo
 mkdir -p .releases
+mkdir -p .releases-done
 
 for chart in $(ls charts); do
     helm package "charts/${chart}" --destination .releases/
+    ls .releases
+    cr upload -r "${CIRCLE_PROJECT_REPONAME}" -o "${CIRCLE_PROJECT_USERNAME}" -p ".releases/" -t "${GITHUB_TOKEN}"
+    mv .releases/${chart}* .releases-done/
 done
 
-git checkout "${BRANCH}"
+mv .cr-index/index.yaml .releases/
+mv .releases-done/* .releases/
 
-helm repo index .releases/ --merge ./index.yaml --url https://filecoin-project.github.io/helm-charts
-
-cr upload --git-repo "${CIRCLE_PROJECT_USERNAME}" --owner "${CIRCLE_PROJECT_REPONAME}" -p .releases/ --token "${GITHUB_TOKEN}"
-cr index --charts-repo https://filecoin-project.github.io/helm-charts --git-repo "${CIRCLE_PROJECT_USERNAME}" --owner "${CIRCLE_PROJECT_REPONAME}" -p releases/ --token "${GITHUB_TOKEN}" --index-path index.yaml
+git checkout -f "${BRANCH}"
+mkdir .cr-index
+mv .releases/index.yaml .cr-index/
+cr index -c https://filecoin-project.github.io/helm-charts -r "${CIRCLE_PROJECT_REPONAME}" -o "${CIRCLE_PROJECT_USERNAME}" -p .releases/ -t "${GITHUB_TOKEN}"
+mv .cr-index/index.yaml .
+rm .cr-index
 
 git config user.email "${GITHUB_EMAIL}"
 git config user.name "${GITHUB_USERNAME}"
-git add index.html
+git add index.yaml
 git commit -m "update helm chart repo index to ${CIRCLE_SHA1}"
 git push https://${GITHUB_TOKEN}@github.com/filecoin-project/helm-charts.git "${BRANCH}"
